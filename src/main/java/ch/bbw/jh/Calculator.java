f199130d94776364b230a97848ffcea8c0d068e6
Jannik <Jannik.Hug@swisscom.com>
2019-12-18 10:26:22 +0100

    Prepare test for subtraktion

--- src/main/java/ch/bbw/jh/Calculator.java
+++ src/main/java/ch/bbw/jh/Calculator.java
@@ -4,4 +4,7 @@ public class Calculator {
     public int summe(int summe1, int summe2){
         return summe1+summe2;
     }
+    public int subtraktion(int value1, int value2){
+        return 0;
+    }
 }

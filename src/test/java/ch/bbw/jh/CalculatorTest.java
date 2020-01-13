c08c71a3ccefefdaae7844dd60ad2104d255ffe5
Jannik <Jannik.Hug@swisscom.com>
2020-01-13 12:59:55 +0100

    implemented 8 different Summe testcases

--- src/test/java/ch/bbw/jh/CalculatorTest.java
+++ src/test/java/ch/bbw/jh/CalculatorTest.java
@@ -20,19 +20,59 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testSubtractionZweiPositiveIsOk(){
-        assertTrue(testee.subtraktion(25, 10) ==15);
+    public void testSummePositivAndNegativIsOk(){
+        assertTrue(testee.summe(-5, 10) ==5);
     }
 
     @Test
-    public void testSummePositivAndNegativIsOk(){
-        assertTrue(testee.summe(-5, 10) ==5);
+    public void testSummePositiveAndNullIsOk(){
+        assertTrue(testee.summe(3, 0) ==3);
+    }
+
+    @Test
+    public void testSummeMAX_VALUEIsOk(){
+        assertTrue(testee.summe(Integer.MAX_VALUE, 3) == Integer.MAX_VALUE+3);
+    }
+
+    @Test
+    public void testSummeMIN_VALUEIsOk(){
+        assertTrue(testee.summe(Integer.MIN_VALUE, 5) == Integer.MIN_VALUE+5);
+    }
+
+    @Test
+    public void testSummeTwoNegativesIsOk(){
+        assertTrue(testee.summe(-23, -12) ==-35);
+    }
+
+    @Test
+    public void testSummeMAX_VALUEAndNegativeIsOk(){
+        assertTrue(testee.summe(Integer.MAX_VALUE, -5) == Integer.MAX_VALUE+-5);
+    }
+
+    @Test
+    public void testSumMIN_VALUEAndNegativeIsOk(){
+        assertTrue(testee.summe(Integer.MIN_VALUE, -6) == Integer.MIN_VALUE+-6);
+    }
+
+
+
+
+
+
+
+    @Test
+    public void testSubtractionZweiPositiveIsOk(){
+        assertTrue(testee.subtraktion(25, 10) ==15);
     }
 
     @Test
     public void testSubtractionPositiveAndNegativeIsOk(){
         assertTrue(testee.subtraktion(-12, 6) ==-18);
     }
+
+
+
+
 
     @Test(expected=ArithmeticException.class)
     public void testExpectedExcpetion(){
